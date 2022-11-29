1927baf08d9bd88369c766e6b4e887254f3ff6d3
oc-garcia <octaviogarcia1988@gmail.com>
2022-11-29 13:03:50 -0300

    Removing red dot from all components

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let ntfCounter = document.getElementById("notification__counter");
 let markAll = document.getElementById("notification__markAsRead");
 
 let unread = document.querySelectorAll("div.notification__card-unread");
-let readRedDot = document.querySelectorAll()
+let redDot = document.querySelectorAll("p.red-dot")
 
 for (let i = 0; i < unread.length; i++) {
   unread[i].addEventListener("click", () => changeStatus(unread[i]));
@@ -13,15 +13,21 @@ markAll.addEventListener("click", () => changeStatusAll(unread));
 function changeStatus(prmt) {
   if (prmt.className == "notification__card-unread") {
     prmt.className = "notification__card-read";
-    //  countNtf();
   } else {
     prmt.className = "notification__card-unread";
-    //   countNtf();
   }
 }
 
 function changeStatusAll(prmt) {
-  for (let i = 0; i < unread.length; i++) {
+  for (let i = 0; i < prmt.length; i++) {
     prmt[i].className = "notification__card-read";
+    removeRedDotAll(redDot)
+  }
+}
+
+function removeRedDotAll(prmt) {
+  for (let i = 0; i < prmt.length; i++) {
+    prmt[i].className = "red-dot-read"
+    
   }
 }
