0c71244e275d2e91c01d87531b5a59e4101ed918
oc-garcia <octaviogarcia1988@gmail.com>
2022-11-29 12:55:48 -0300

    Mark all as read working

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 let ntfCounter = document.getElementById("notification__counter");
-let markAll = document.getElementById("notifiction__markAsRead");
+let markAll = document.getElementById("notification__markAsRead");
 
 let unread = document.querySelectorAll("div.notification__card-unread");
+let readRedDot = document.querySelectorAll()
 
 for (let i = 0; i < unread.length; i++) {
   unread[i].addEventListener("click", () => changeStatus(unread[i]));
 }
 
+markAll.addEventListener("click", () => changeStatusAll(unread));
+
 function changeStatus(prmt) {
   if (prmt.className == "notification__card-unread") {
     prmt.className = "notification__card-read";
-  //  countNtf();
+    //  countNtf();
   } else {
     prmt.className = "notification__card-unread";
- //   countNtf();
+    //   countNtf();
   }
 }
 
 function changeStatusAll(prmt) {
-  prmt.className = "notification__card-read";
+  for (let i = 0; i < unread.length; i++) {
+    prmt[i].className = "notification__card-read";
+  }
 }
-
-function countNtf() {}
