3bbbd25795365d5bb0a298c796e5beb72e622fcc
oc-garcia <octaviogarcia1988@gmail.com>
2022-11-29 16:11:56 -0300

    Notification counter implemented

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-let ntfCounter = document.getElementById("notification__counter".innerHTML);
+let ntfCounter = document.getElementById("notification__counter");
 let markAll = document.getElementById("notification__markAsRead");
 
 let unread = document.querySelectorAll("div.notification__card-unread");
 let redDot = document.querySelectorAll("p.red-dot");
+
+let x = parseInt(unread.length);
 
 for (let i = 0; i < unread.length; i++) {
   unread[i].addEventListener("click", () => changeStatus(unread[i], redDot[i]));
@@ -16,13 +18,13 @@ function changeStatus(prmt1, prmt2) {
   if (prmt1.className == "notification__card-unread") {
     prmt1.className = "notification__card-read";
     prmt2.className = "red-dot-read";
-    ntfCounter -= ntfCounter;
-    ntfCounter.innerHTML = ntfCounter;
+    x -= 1;
+    ntfCounter.innerHTML = `<p>${x}</p>`;
   } else {
     prmt1.className = "notification__card-unread";
     prmt2.className = "red-dot";
-    ntfCounter += ntfCounter;
-    ntfCounter.innerHTML = ntfCounter;
+    x += 1;
+    ntfCounter.innerHTML = `<p>${x}</p>`;
   }
 }
 
@@ -30,23 +32,13 @@ function changeStatusAll(prmt) {
   for (let i = 0; i < prmt.length; i++) {
     prmt[i].className = "notification__card-read";
     removeRedDotAll(redDot);
+    x = 0;
+    ntfCounter.innerHTML = `<p>${x}</p>`;
   }
 }
 
 function removeRedDotAll(prmt) {
   for (let i = 0; i < prmt.length; i++) {
     prmt[i].className = "red-dot-read";
-  }
-}
-
-function counter() {
-  for (let i = 0; i < unread.length; i++) {
-    let x = 0;
-    const counter = unread[i];
-    while (counter.className == "notification__card-unread") {
-      x = x + 1;
-      //ntfCounter.innerHTML = `<p id="notification__counter"> ${x} </p>`;
-      return unread[i];
-    }
   }
 }
