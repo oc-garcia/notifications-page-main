2c7a6c56acce408cdb8d311d1c82228e2229b310
oc-garcia <octaviogarcia1988@gmail.com>
2022-11-29 15:07:48 -0300

    quick fixes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let ntfCounter = parseInt(document.getElementById("notification__counter").innerHTML);
+let ntfCounter = document.getElementById("notification__counter".innerHTML);
 let markAll = document.getElementById("notification__markAsRead");
 
 let unread = document.querySelectorAll("div.notification__card-unread");
@@ -10,7 +10,7 @@ for (let i = 0; i < unread.length; i++) {
 
 markAll.addEventListener("click", () => changeStatusAll(unread));
 
-//ntfCounter.addEventListener("load", () => counter(ntfCounter))
+//ntfCounter.addEventListener("click", counter);
 
 function changeStatus(prmt1, prmt2) {
   if (prmt1.className == "notification__card-unread") {
@@ -36,5 +36,17 @@ function changeStatusAll(prmt) {
 function removeRedDotAll(prmt) {
   for (let i = 0; i < prmt.length; i++) {
     prmt[i].className = "red-dot-read";
+  }
+}
+
+function counter() {
+  for (let i = 0; i < unread.length; i++) {
+    let x = 0;
+    const counter = unread[i];
+    while (counter.className == "notification__card-unread") {
+      x = x + 1;
+      //ntfCounter.innerHTML = `<p id="notification__counter"> ${x} </p>`;
+      return unread[i];
+    }
   }
 }
